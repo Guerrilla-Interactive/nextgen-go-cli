6204e422e223133ddde551ff6f73838aab142b8e
Frikk Jarl <Frikk@jarl.email>
2025-01-13 03:56:22 +0100

    Update cli.js

--- bin/cli.js
+++ bin/cli.js
@@ -6,9 +6,9 @@ const os = require('os');
 const platform = os.platform();
 
 let binary;
-if (platform === 'darwin') binary = 'nextgen-go-cli_darwin_amd64_v1/nextgen-go-cli';
-else if (platform === 'win32') binary = 'nextgen-go-cli_windows_amd64_v1/nextgen-go-cli.exe';
-else if (platform === 'linux') binary = 'nextgen-go-cli_linux_amd64_v1/nextgen-go-cli';
+if (platform === 'darwin') binary = 'bin/cli-macos';
+else if (platform === 'win32') binary = 'bin/cli-win.exe';
+else if (platform === 'linux') binary = 'bin/cli-linux';
 else {
   console.error(`Unsupported platform: ${platform}`);
   process.exit(1);
