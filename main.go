67853df55ab8132cd8eb82e4e20e868e88200a38
Frikk Jarl <Frikk@jarl.email>
2025-09-02 12:55:34 +0200

    chore: Release v1.0.82

--- main.go
+++ main.go
@@ -37,7 +37,7 @@ import (
 )
 
 // Define Version (will be set via linker flags during build)
-var Version = "v1.0.81"
+var Version = "v1.0.82"
 
 // Add a new message type that will trigger quit after a delay.
 type QuitAfterDelayMsg struct{}
