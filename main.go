c34853d446c81f39a132e09c054db31fec233914
Frikk Jarl <Frikk@jarl.email>
2025-09-05 11:57:38 +0200

    chore: Release v1.0.104

--- main.go
+++ main.go
@@ -37,7 +37,7 @@ import (
 )
 
 // Define Version (will be set via linker flags during build)
-var Version = "v1.0.103"
+var Version = "v1.0.104"
 
 // determine CLI name variants (primary + aliases)
 func detectPrimaryCLIName() string {
