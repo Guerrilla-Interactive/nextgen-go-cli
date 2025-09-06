3e05e2ecbeeb808f46c963361828fe5ca9f884f2
Frikk Jarl <Frikk@jarl.email>
2025-09-06 17:27:11 +0200

    chore: Release v1.0.109

--- main.go
+++ main.go
@@ -37,7 +37,7 @@ import (
 )
 
 // Define Version (will be set via linker flags during build)
-var Version = "v1.0.108"
+var Version = "v1.0.109"
 
 // exitLog holds a log-style summary printed after the TUI exits.
 var exitLog string
